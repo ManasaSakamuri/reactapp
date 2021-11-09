c65757ab8f9f44c747a43f13f685decede1bef24
manasasakamuri <sakamurimanasa7176@gmail.com>
2021-11-09 16:01:53 +0530

    initial commit

---
+++ src/components/ClassClick.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+export class ClassClick extends Component {
+    clickHandler(){
+        console.log('clicked from ClassClick')
+    }
+    render() {
+        return (
+            <div>
+                <button onClick = {this.clickHandler}>click me class</button>
+            </div>
+        )
+    }
+}
---
+++ src/components/Great.js
@@ -0,0 +1,6 @@
+import React from 'react'
+
+function Great(){
+    return <h1>welcome Manasa</h1>
+}
+export default Great
