83b026531e57290aef5401b215934b258bc67d98
manasasakamuri <sakamurimanasa7176@gmail.com>
2021-11-09 16:17:50 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -4,11 +4,12 @@ import Great from './components/Great';
 import { ClassClick } from './components/ClassClick';
 import ParentComponent from './components/ParentComponent';
 import ChildComponent from './components/ChildComponent';
+import Counter from './components/Counter';
 
 function App() {
   return (
     <div className="App">
-      <ChildComponent/>
+      <Counter/>
     </div>
   );
 }
