2568e4681f9af5bb6f715fdb85d21996c12762f3
manasasakamuri <sakamurimanasa7176@gmail.com>
2021-11-09 16:22:40 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -5,11 +5,12 @@ import { ClassClick } from './components/ClassClick';
 import ParentComponent from './components/ParentComponent';
 import ChildComponent from './components/ChildComponent';
 import Counter from './components/Counter';
+import EventBind from './components/EventBind';
 
 function App() {
   return (
     <div className="App">
-      <Counter/>
+      <EventBind/>
     </div>
   );
 }
