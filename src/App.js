7410d0d5e26a5b1aa156072a89883040d95d7bd6
manasasakamuri <sakamurimanasa7176@gmail.com>
2021-11-10 15:24:56 +0530

    form handling

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
-import Great from './components/Great';
+
 import { ClassClick } from './components/ClassClick';
 
 import Person from './components/Person';
 import Stylesheet from './components/StyleSheet';
-import Table from './components/Table';
+import Form from './components/Form';
 
 function App() {
   return (
     <div className="App">
-      <Table/>
-    </div>
+      <Form/>
+      </div>
   );
 }
 
