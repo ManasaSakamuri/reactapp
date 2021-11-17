1cb2fe4d8b20db11e064ed96ccd714f3de1385e2
manasasakamuri <sakamurimanasa7176@gmail.com>
2021-11-17 15:42:55 +0530

    hooks

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
-import HookCounter3 from './components/hooks/HookCounter3';
+import CounterHook1 from './components/hooks/useeffect/CounterHook1';
 
 function App() {
   return (
     <div className="App">
-      <HookCounter3/>
+      <CounterHook1/>
       </div>
   );
 }
