7815c0c8e51ece6028221b1745edd93d484ec17b
manasasakamuri <sakamurimanasa7176@gmail.com>
2021-11-17 12:50:19 +0530

    refs

--- src/App.js
+++ src/App.js
@@ -2,17 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 
 import { ClassClick } from './components/ClassClick';
-import Table from './components/Table';
-import PortalDemo from './components/portals/PortalDemo';
-import ClassCounter from './components/hooks/ClassCounter';
-import HookCounter from './components/hooks/HookCounter';
-import Greet from './components/Greet';
 import welcome from './components/Welcome';
+import RefsDemo from './components/refs/RefsDemo';
+import FrParentInput from './components/refs/FrParentInput';
 
 function App() {
   return (
     <div className="App">
-      <welcome/>
+      <FrParentInput/>
       </div>
   );
 }
--- src/components/Greet.js
+++
@@ -1,6 +0,0 @@
-import React from 'react'
-
-function Greet(){
-    return <h1>welcome Manasa</h1>
-}
-export default Greet
